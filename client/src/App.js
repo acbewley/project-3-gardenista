543c26a9c58501249e6cde273207b9569a28d5f3
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-11 19:58:08 -0500

    fixed relative file paths to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Footer from "./components/Footer/index";
+import Header from "./components/Header/index";
 import Home from "./pages/Home";
 import Landing from "./pages/Landing";
 import Journal from "./pages/Journal";
