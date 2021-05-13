7daabcf0fc6cebe9706e5b9f09493f669d134060
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-13 18:21:04 -0500

    fixed inline error in Journal and added plants card styling file

--- client/src/components/PlantCard/index.js
+++ client/src/components/PlantCard/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../PlantCard/style.css"
 
 export function PlantCard({
     name,
--- client/src/pages/PlantCards.js
+++ client/src/pages/PlantCards.js
@@ -14,9 +14,7 @@ function PlantCards() {
           
       </Col>
       <Col size="md-6">
-        <PlantCard>
-          Test
-        </PlantCard>
+      <PlantCard name="test" scientific_name="testing" image="https://www.theenglishgarden.co.uk/wp-content/uploads/2017/10/Paeonia-suffruticosa-Hana-Kisoi.jpg" description="put description here" />
       </Col>
     </Row>
   </Container>
