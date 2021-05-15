7721a9a6bacea482b1be40e4699891b144863dde
acbewley <acbewley13@gmail.com>
2021-05-15 11:43:32 -0400

    made homepage mobile responsive'

--- client/src/components/Weather/index.js
+++ client/src/components/Weather/index.js
@@ -3,7 +3,7 @@ import './style.css'
 
 export function WeatherDiv({ children }) {
   return (
-    <div className="weather-div" style={{ marginTop: '90px' }}>
+    <div className="weather-div" style={{ width: "100%", height: "100%", marginTop: '90px' }}>
       {children}
     </div>
   )
@@ -15,9 +15,9 @@ export function WeatherCard(props) {
     return (
       <div className='row'>
         {props.data.slice(0, 5).map(day => (
-          <div className="card col" style={{ margin: '20px', width: "100px", backgroundColor: "#A3C586" }}>
+          <div className="card col-md " style={{ width: "90%", margin: "20px", backgroundColor: "#A3C586" }}>
             <div className="card-body">
-              <h5 className="card-title" style={{ fontSize: '12pt' }}>{new Date(day.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' })}</h5>
+              <h5 className="card-title" style={{ fontSize: '10pt' }}>{new Date(day.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' })}</h5>
               <p className="card-text" style={{ fontSize: '9pt' }}>Temperature: {day.temp.day}°</p>
               <p className="card-text" style={{ fontSize: '9pt' }}>Humidity: {day.humidity}%</p>
               <p className="card-text" style={{ fontSize: '9pt' }}>Chance of rain: {day.pop}%</p>
