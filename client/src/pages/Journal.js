6f8c984a8da6d684e2d45e7eccfa77208da4235c
tuanhoangle <tuanleprofessional@gmail.com>
2021-05-15 11:32:59 -0400

    user back-end fix

--- client/src/pages/Journal.js
+++ client/src/pages/Journal.js
@@ -1,21 +1,19 @@
-import React from "react"
+import React from "react";
 import { Container, Row, Col } from "../components/Grid/index";
-import Entry from "../components/Entry"
+import Entry from "../components/Entry";
 // import entryList from "../components/entryList"
 
-function Journal () {
-    return (
-        <Container>
-          <Row>
-            <Col size="md-3">
-            Journal Entries
-            </Col>
-            <Col size="md-9">
-            <Entry></Entry>
-            </Col>
-          </Row>
-        </Container>
-    )
+function Journal() {
+  return (
+    <Container>
+      <Row>
+        <Col size="md-3">Journal Entries</Col>
+        <Col size="md-9">
+          <Entry></Entry>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Journal;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -18,12 +18,17 @@ export default {
     return axios.post("/api/plants", plantData);
   },
 
-  getWeather: function(lat, long) {
-    return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,minutely,hourly,alerts&units=imperial&appid=c1ba9a1d8d332b1ef324e57e7f47dbdf`)
+  getWeather: function (lat, long) {
+    return axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,minutely,hourly,alerts&units=imperial&appid=c1ba9a1d8d332b1ef324e57e7f47dbdf`
+    );
   },
 
   getUsers: function () {
     return axios.get("/api/user/");
   },
 
+  getUser: function (id) {
+    return axios.get("/api/user/" + id);
+  },
 };
