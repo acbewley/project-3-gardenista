5110bec84da956afe2c06680b90adbd1c9b5d82e
acbewley <acbewley13@gmail.com>
2021-05-22 11:54:32 -0400

    added next water date functionality

--- scripts/seedsUser.js
+++ scripts/seedsUser.js
@@ -17,6 +17,7 @@ const userSeed = [
           "https://www.gardenia.net/storage/app/public/uploads/images/sxToWePBHwBLxedmnXiUcXvqCqd8VEKL3BuhQ1at.jpeg",
         description:
           "Abelia is a very durable and adaptable shrub with dense, glossy dark green foliage. It produces hundreds of 1 inch funnel shaped flowers from early summer until the first frost",
+        next_water: ""
       },
       {
         name: "Algerian Ivy",
@@ -27,6 +28,7 @@ const userSeed = [
           "https://www.architecturalplants.com/wp-content/uploads/2020/10/1277790506.jpg",
         description:
           "African Daisies typically have bold, beautiful foliage and large, 3-4 inches, intensely colored flowers with bright centers. The foliage is gray to silvery green and coated with soft, downy silver hairs. The flowers close at night.",
+        next_water: ""
       },
       {
         name: "Flowering Onion",
@@ -37,6 +39,7 @@ const userSeed = [
           "https://static.onecms.io/wp-content/uploads/sites/37/2020/03/allium-aflatunense-summer-bulbs-6ea11821.jpg",
         description:
           "Flowering Onions are bulb type plants with green, grass-like foliage. They produce hollow, leafless stems that are topped with 1-5inch flower clusters that can contain as many as 100 tiny, star shaped flowers.",
+        next_water: ""
       },
       {
         name: "Alpine Aster",
@@ -47,6 +50,7 @@ const userSeed = [
           "https://www.gardenia.net/storage/app/public/uploads/images/detail/MkO7Tw6C5isTWodSVHBseYFfS4cAmq7lTsecmtNO.jpeg",
         description:
           "The Alpine Aster plant forms a nice compact mound and produces an abundance of 1 ½ inch white flowers in May and June",
+        next_water: ""
       },
       {
         name: "Alpine Poppy",
@@ -57,6 +61,7 @@ const userSeed = [
           "https://upload.wikimedia.org/wikipedia/commons/5/57/Papaver_alpinum_subsp_alpinum_s_str_%28AlpMohn%29_IMG_3067.jpg",
         description:
           "This short lived, miniature version of the Iceland Poppy readily re seeds itself. It produces saucer shaped 1in. white, yellow or peach flowers in mid summer",
+        next_water: ""
       },
       {
         name: "American Dune Grass",
@@ -67,6 +72,7 @@ const userSeed = [
           "https://www.thompson-morgan.com/product_images/100/optimised/LEY4515-A_h.jpg",
         description:
           "American Dune Grass is a valuable plant for the prevention of the wind eroding your soil due to its rhizomatous spreading habit",
+        next_water: ""
       },
       {
         name: "Aubrieta",
@@ -77,6 +83,7 @@ const userSeed = [
           "https://plantcaretoday.com/wp-content/uploads/aubrieta-deltoidea-1200-630-FB-06302019-min.jpg",
         description:
           "Common Aubrieta is a vigorous growing, mat forming ground cover and rock garden plant. It is tolerant of a wide range of conditions, and rewards you with a long lasting cover of delicate flowers in early Spring.",
+        next_water: ""
       },
       {
         name: "Azalea",
@@ -87,6 +94,7 @@ const userSeed = [
           "https://static.onecms.io/wp-content/uploads/sites/37/2020/03/23/pink-purple-rhododendron-7d131722.jpg",
         description:
           "Azaleas must be grown in an acid soil, with a pH of 5.0 to 6.5. The buds for next years blooms will be formed this year, so it important that the faded blooms are removed",
+        next_water: ""
       },
       {
         name: "Bearded Iris",
@@ -97,6 +105,7 @@ const userSeed = [
           "https://www.whiteflowerfarm.com/mas_assets/cache/image/6/4/8/2/25730.Jpg",
         description:
           "Bearded Iris are a rhizomous type of Iris should be planted fertile, well drained soil where they will receive 5-8 hours of sun each day. They are available in almost every color imaginable.",
+        next_water: ""
       },
       {
         name: "Bears Breeches",
@@ -107,6 +116,7 @@ const userSeed = [
           "https://upload.wikimedia.org/wikipedia/commons/7/7a/Acanthus_montanus3.jpg",
         description:
           "Bear's Breeches are huge, mound forming perennials with dramatic, deeply cut, bright green leaves that can reach one foot wide and two feet long. From late spring until mid summer, they produce impressive, 2-3 foot tall spikes of hooded, white or light pink flowers.",
+        next_water: ""
       },
     ],
   },
@@ -123,6 +133,7 @@ const userSeed = [
           "https://www.thompson-morgan.com/product_images/100/optimised/LEY4515-A_h.jpg",
         description:
           "American Dune Grass is a valuable plant for the prevention of the wind eroding your soil due to its rhizomatous spreading habit",
+        next_water: ""
       },
       {
         name: "Aubrieta",
@@ -133,6 +144,7 @@ const userSeed = [
           "https://plantcaretoday.com/wp-content/uploads/aubrieta-deltoidea-1200-630-FB-06302019-min.jpg",
         description:
           "Common Aubrieta is a vigorous growing, mat forming ground cover and rock garden plant. It is tolerant of a wide range of conditions, and rewards you with a long lasting cover of delicate flowers in early Spring.",
+        next_water: ""
       },
     ],
   },
