730c96c548fc07dc45e4e4d5e68eb5fdf96dde94
tuanhoangle <tuanleprofessional@gmail.com>
2021-05-22 10:55:01 -0400

    auto login

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,25 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useUserContext } from "../../utils/globalState";
-import img from '../../images/gardenista-3.png';
+import img from "../../images/gardenista-3.png";
+import { useHistory } from "react-router-dom";
 
 const Header = () => {
-  let isLoggin = localStorage.getItem('isLoggin') === 'true'
+  let isLoggin = localStorage.getItem("isLoggin") === "true";
+  const history = useHistory();
 
   function logOut() {
-    localStorage.setItem('isLoggin', false);
-    localStorage.setItem('user', undefined)
-    window.location.pathname = '/'
+    localStorage.setItem("isLoggin", false);
+    localStorage.setItem("user", undefined);
+    window.location.pathname = "/";
   }
 
   useEffect(() => {
-    isLoggin = localStorage.getItem('isLoggin') === 'true'
-  })
+    isLoggin = localStorage.getItem("isLoggin") === "true";
+    toHome();
+  });
+
+  function toHome() {
+    if (isLoggin) {
+      history.push("/home");
+    }
+  }
 
   function authenticate() {
-    console.log('woah')
+    console.log("woah");
     if (isLoggin) {
       return (
-        <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#5b7444" }}>
+        <nav
+          className="navbar navbar-expand-lg navbar-light"
+          style={{ backgroundColor: "#5b7444" }}
+        >
           <div className="container-fluid">
             <a className="navbar-brand" href="/home">
               <img src={img} alt="" width="75" height="93.75" />
@@ -27,36 +39,63 @@ const Header = () => {
             <div className="collapse navbar-collapse" id="navbarText">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link" style={{ color: "white" }} href="/cards">Your Plants</a>
+                  <a
+                    className="nav-link"
+                    style={{ color: "white" }}
+                    href="/cards"
+                  >
+                    Your Plants
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" style={{ color: "white" }} href="/plants">Add Plants</a>
+                  <a
+                    className="nav-link"
+                    style={{ color: "white" }}
+                    href="/plants"
+                  >
+                    Add Plants
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" style={{ color: "white" }} href="/journal">Journal</a>
+                  <a
+                    className="nav-link"
+                    style={{ color: "white" }}
+                    href="/journal"
+                  >
+                    Journal
+                  </a>
                 </li>
               </ul>
               <span className="nav-item">
-                <a className="nav-link" style={{ color: "white" }} onClick={logOut}>Log Out</a>
+                <a
+                  className="nav-link"
+                  style={{ color: "white" }}
+                  onClick={logOut}
+                >
+                  Log Out
+                </a>
               </span>
             </div>
           </div>
         </nav>
-      )
+      );
     } else {
       return (
-        <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#5b7444" }}>
+        <nav
+          className="navbar navbar-expand-lg navbar-light"
+          style={{ backgroundColor: "#5b7444" }}
+        >
           <div className="container-fluid">
             <a className="navbar-brand">
               <img src={img} alt="" width="75" height="93.75" />
             </a>
           </div>
         </nav>
-      )
+      );
     }
   }
 
-  return (authenticate());
-}
+  return authenticate();
+};
 
 export default Header;
