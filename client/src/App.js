c177b59450f8801ac700f2443915d2dae3d705c1
acbewley <acbewley13@gmail.com>
2021-05-25 22:23:48 -0400

    j

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from "./components/Footer";
+import Footer from "src/components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Landing from "./pages/Landing";
