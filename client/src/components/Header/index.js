953b350b0e6b5eb351cf3f4e84345f661d52c0b0
acbewley <acbewley13@gmail.com>
2021-05-26 20:32:06 -0400

    responsive header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
-import { useUserContext } from "../../utils/globalState";
 import img from "../../images/gardenista-3.png";
 import API from "../../utils/API";
 import { useHistory, useLocation } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
   let isLoggin = true;
@@ -56,58 +56,23 @@ const Header = () => {
       );
     } else {
       return (
-        <nav
-          className="navbar navbar-expand-lg navbar-light"
-          style={{ backgroundColor: "#5b7444" }}
-        >
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/home">
-              <img src={img} alt="" width="75" height="93.75" />
-            </a>
-            <div className="collapse navbar-collapse" id="navbarText">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    style={{ color: "white" }}
-                    href="/cards"
-                  >
-                    Your Plants
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    style={{ color: "white" }}
-                    href="/plants"
-                  >
-                    Add Plants
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    style={{ color: "white" }}
-                    href="/journal"
-                  >
-                    Journal
-                  </a>
-                </li>
-              </ul>
-              <span className="nav-item">
-                <a
-                  className="nav-link"
-                  style={{ color: "white" }}
-                  onClick={logOut}
-                >
-                  Log Out
-                </a>
-              </span>
-            </div>
-          </div>
-        </nav>
+        <Navbar style={{ backgroundColor: '#5b7444' }} variant="dark" className="color-nav" expand="lg">
+          <Navbar.Brand href="/home"><img src={img} width="75" height="93.75" /></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/cards">Your Plants</Nav.Link>
+              <Nav.Link href="/plants">Add Plants</Nav.Link>
+              <Nav.Link href="/journal">Journal</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link onClick={logOut}>Log Out</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       );
     }
+
   }
 
   return authenticate();
